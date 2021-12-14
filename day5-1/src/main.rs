6cc765a50123b6eae6c9ed125e3520bdf92777ff
Peter Evers <pevers90@gmail.com>
2021-12-14 07:58:40 +0100

    optimize day5-1

--- day5-1/src/main.rs
+++ day5-1/src/main.rs
@@ -7,19 +7,15 @@ fn main() {
             .split(" -> ")
             .flat_map(|c| c.split(",").map(|c| c.parse().unwrap()))
             .collect();
-        // Vertical line
-        if coords[0] == coords[2] {
-            let min = coords[1].min(coords[3]);
-            let max = coords[1].max(coords[3]);
-            (min..max + 1).for_each(|y| {
-                *map.entry((coords[0], y)).or_insert(0) += 1;
-            });
-        } else if coords[1] == coords[3] {
-            let min = coords[0].min(coords[2]);
-            let max = coords[0].max(coords[2]);
-            (min..max + 1).for_each(|x| {
-                *map.entry((x, coords[1])).or_insert(0) += 1;
-            });
+        let ((x, y), (xx, yy)) = (
+            (coords[0].min(coords[2]), coords[1].min(coords[3])),
+            (coords[0].max(coords[2]), coords[1].max(coords[3])),
+        );
+        if x == xx {
+            (y..=yy).for_each(|y| *map.entry((x, y)).or_insert(0) += 1);
+        }
+        if y == yy {
+            (x..=xx).for_each(|x| *map.entry((x, y)).or_insert(0) += 1);
         }
     });
     println!("{}", map.values().filter(|&v| *v >= 2).count());
