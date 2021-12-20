0bec314821baf841ff50970f21f7f7a9c1d3b783
Peter Evers <pevers90@gmail.com>
2021-12-20 10:32:10 +0100

    day-11

---
+++ day11-2/src/main.rs
@@ -0,0 +1,59 @@
+use std::process::exit;
+
+// Quick and dirty solution
+
+const NEIGHBORS: [(isize, isize); 8] = [
+    (0, -1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+    (0, 1),
+    (-1, 1),
+    (-1, 0),
+    (-1, -1),
+];
+
+fn main() {
+    let mut map = include_bytes!("../input")
+        .split(|&b| b == b'\n')
+        .map(|l| l.to_vec())
+        .collect::<Vec<_>>();
+    let mut flashes = 0;
+    (0..10000).for_each(|step| {
+        for y in 0..map.len() {
+            for x in 0..map[0].len() {
+                map[y][x] += 1;
+            }
+        }
+        loop {
+            for y in 0..map.len() {
+                for x in 0..map[0].len() {
+                    if map[y][x] > b'9' {
+                        map[y][x] = b'0';
+                        flashes += 1;
+
+                        NEIGHBORS
+                            .iter()
+                            .map(|(xx, yy)| {
+                                ((x as isize + xx) as usize, (y as isize + yy) as usize)
+                            })
+                            .for_each(|(x, y)| match map.get(y).and_then(|l| l.get(x)) {
+                                Some(b'0') => {}
+                                Some(_) => map[y][x] += 1,
+                                _ => {}
+                            });
+                    }
+                }
+            }
+
+            if map.iter().all(|l| l.iter().all(|&c| c <= b'9')) {
+                break;
+            }
+        }
+        // Check if synchronized
+        if map.iter().all(|l| l.iter().all(|&c| c == b'0')) {
+            println!("synced at: {}", step + 1);
+            exit(0);
+        }
+    });
+}
