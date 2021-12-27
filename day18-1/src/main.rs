c17c8bec264a064b7ac3a04fca59eca9508e0e3b
Peter Evers <pevers90@gmail.com>
2021-12-27 09:29:27 +0100

    day 18

--- day18-1/src/main.rs
+++ day18-1/src/main.rs
@@ -1,102 +1,158 @@
-use nom::{
-    branch::alt,
-    bytes::complete::tag,
-    character::complete::digit1,
-    combinator::map,
-    sequence::{delimited, separated_pair},
-    IResult,
-};
-use std::{cell::RefCell, rc::Rc};
+#![feature(int_roundings)]
 
-#[derive(Debug, PartialEq, Clone)]
-struct Node {
-    value: i8,
-    left: Option<NodeType>,
-    right: Option<NodeType>,
+// I first tried to do it building
+// a balanced graph but that didn't really work out
+
+type Snail = (u8, i8);
+
+fn parse(input: &str) -> Vec<Snail> {
+    let mut depth = 0;
+    let mut result = Vec::new();
+    input.as_bytes().iter().for_each(|&c| match c {
+        b'[' => {
+            depth = depth + 1;
+        }
+        b']' => {
+            depth = depth - 1;
+        }
+        b',' => {}
+        _ => {
+            result.push((depth, (c - b'0') as i8));
+        }
+    });
+    result
 }
 
-type NodeType = Rc<RefCell<Node>>;
-
-fn digit(i: &str) -> IResult<&str, Node> {
-    map(digit1, |s: &str| Node {
-        value: s.parse().unwrap(),
-        left: None,
-        right: None,
-    })(i)
-}
-
-fn expr(i: &str) -> IResult<&str, (Node, Node)> {
-    separated_pair(alt((parens, digit)), tag(","), alt((parens, digit)))(i)
-}
-
-fn parens(i: &str) -> IResult<&str, Node> {
-    map(delimited(tag("["), expr, tag("]")), |(left, right)| Node {
-        value: -1,
-        left: Some(Rc::new(RefCell::new(left))),
-        right: Some(Rc::new(RefCell::new(right))),
-    })(i)
-}
-
-fn parse(i: &str) -> Node {x
-    let (_, node) = parens(i).unwrap();
-    node
-}
-
-fn find_leftmost_nested(node: &NodeType, times: u32) -> Option<NodeType> {
-    if times == 1 {
-        return Some(node.clone());
+fn explode(snail: &mut Vec<Snail>) {
+    let mut i = 0;
+    while i < snail.len() {
+        if snail[i].0 == 5 {
+            if i > 0 && snail.get(i - 1).is_some() {
+                snail[i - 1].1 += snail[i].1;
+            }
+            if i < snail.len() - 2 && snail.get(i + 2).is_some() {
+                snail[i + 2].1 += snail[i + 1].1;
+            }
+            snail.drain(i..=i + 1);
+            snail.insert(i, (4, 0));
+            i = 0;
+        } else {
+            i += 1;
+        }
     }
-    let node = node.as_ref().borrow();
-    if let Some(left) = &node.left {
-        return find_leftmost_nested(&left.clone(), times - 1);
-    }
-    if let Some(right) = &node.right {
-        return find_leftmost_nested(&right.clone(), times - 1);
-    }
-    None
 }
 
-fn add(left: Node, right: Node) -> Node {
-    let sum = Node {
-        value: -1,
-        left: Some(Rc::new(RefCell::new(left))),
-        right: Some(Rc::new(RefCell::new(right))),
-    };
-    sum
+fn split(snail: &mut Vec<Snail>) {
+    let mut i = 0;
+    while i < snail.len() {
+        if snail[i].1 >= 10 {
+            let val = snail[i].1;
+            let depth = snail[i].0;
+            snail.insert(i, (depth + 1, val / 2));
+            snail[i + 1].0 = depth + 1;
+            snail[i + 1].1 = val.unstable_div_ceil(2);
+            break;
+        } else {
+            i += 1;
+        }
+    }
+}
+
+fn magnitude(snail: &Vec<Snail>, depth: u8, i: &mut usize) -> u64 {
+  let lhs = 3 * if snail[*i].0 == depth {
+    *i += 1;
+    snail[*i - 1].1 as u64
+  } else {
+    magnitude(snail, depth + 1, i)
+  };
+  let rhs = 2 * if snail[*i].0 == depth {
+    *i += 1;
+    snail[*i - 1].1 as u64
+  } else {
+    magnitude(snail, depth + 1, i)
+  };
+  lhs + rhs
+}
+
+fn reduce(snail: &mut Vec<Snail>) {
+    loop {
+        explode(snail);
+        let len = snail.len();
+        split(snail);
+        if len == snail.len() {
+            break;
+        }
+    }
 }
 
 fn main() {
-    include_str!("../input").split("\n").fold(
-        Node {
-            value: -1,
-            left: None,
-            right: None,
-        },
-        |accum, curr| {
-            let snail = parse(curr);
-            add(snail, accum)
-        },
-    );
+    let content = include_str!("../input")
+        .split("\n")
+        .fold(vec![], |mut accum, curr| {
+            let mut rhs: Vec<Snail> = parse(curr);
+            if accum.len() > 0 {
+              accum = accum.iter().map(|&(d,v)| (d+1, v)).collect();
+              rhs = rhs.iter().map(|&(d, v)| (d+1, v)).collect();
+            }
+            accum.extend(rhs);
+            reduce(&mut accum);
+            accum
+        });
+    println!("{:?}", magnitude(&content, 1, &mut 0));
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{cell::RefCell, rc::Rc};
-
-    use crate::{find_leftmost_nested, parens, parse};
+    use crate::{explode, parse, split};
 
     #[test]
-    fn test_parse() {
-        let input = "[[1,2],[2,3]]";
-        println!("{:?}", parens(input).unwrap());
+    fn test_parse_snail() {
+        let snail = parse("[[[[[9,8],1],2],3],4]");
+        assert_eq!(vec![(5, 9), (5, 8), (4, 1), (3, 2), (2, 3), (1, 4)], snail);
     }
 
     #[test]
-    fn test_leftmost_pair() {
-        let input = "[[[[[9,8],1],2],3],4]";
-        let node = parse(&input);
-        let link = Rc::new(RefCell::new(node));
-        let nested_pair = find_leftmost_nested(&link, 5).unwrap();
-        println!("{:?}", &nested_pair);
+    fn test_explode() {
+        let mut snail = parse("[[[[[9,8],1],2],3],4]");
+        explode(&mut snail);
+        assert_eq!(vec![(4, 0), (4, 9), (3, 2), (2, 3), (1, 4)], snail);
+    }
+
+    #[test]
+    fn test_explode_two() {
+        let mut snail = parse("[7,[6,[5,[4,[3,2]]]]]");
+        explode(&mut snail);
+        assert_eq!(vec![(1, 7), (2, 6), (3, 5), (4, 7), (4, 0)], snail);
+    }
+
+    #[test]
+    fn test_split() {
+        let mut snail = vec![
+            (4, 0),
+            (4, 7),
+            (3, 4),
+            (4, 7),
+            (4, 8),
+            (4, 0),
+            (4, 13),
+            (2, 1),
+            (2, 1),
+        ];
+        split(&mut snail);
+        assert_eq!(
+            vec![
+                (4, 0),
+                (4, 7),
+                (3, 4),
+                (4, 7),
+                (4, 8),
+                (4, 0),
+                (4, 6),
+                (4, 7),
+                (2, 1),
+                (2, 1)
+            ],
+            snail
+        );
     }
 }
